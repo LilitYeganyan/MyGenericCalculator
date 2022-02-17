6295792cc6b66b8c16fec7b96d6908d05efa86a6
lilit.eganyanel53@gmail.com <lilit.eganyanel53@gmail.com>
2022-02-17 23:59:08 +0400

    Last Changes

---
+++ MyGenericCalculator/Operations/Sum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGenericCalculator
+{
+    internal class Sum
+    {
+            public double value { get; set; }
+            public double Operate(double firstParam, double secondParam)
+            {
+                return this.value = firstParam + secondParam;
+            }
+
+    }
+}
